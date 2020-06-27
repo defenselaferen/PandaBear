15349daeb838374f6ecc512dcfe0046eeefe0cfd
thekotekjournal <63573571+thekotekjournal@users.noreply.github.com>
2020-06-27 14:28:10 +0700

    Make Update With This File
    UpdateV2.0

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,9 +4,6 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		echo view('Home/index');
 	}
-
-	//--------------------------------------------------------------------
-
 }
