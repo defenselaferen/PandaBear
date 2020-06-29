cabb2bbc1da2f1a2f21c642451349dbadd08c937
thekotekjournal <63573571+thekotekjournal@users.noreply.github.com>
2020-06-29 16:04:09 +0700

    Update index.php

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -7,45 +7,9 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
-    <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.js" integrity="sha256-L/S5mZS8SPJVWoKym3Zwgf2f3/s3X3MaZ7QBA9HdnN8=" crossorigin="anonymous"></script>
     <title>Panda Bear | Home</title>
   </head>
   <body>
-    <script type="text/babel">
-      class Headers extends React.Component{
-          render(){
-              return(
-                <h1>Hello World</h1>
-            )
-          }
-      };
-      class Header extends React.Component{
-          render(){
-              return(
-                <nav class="navbar navbar-expand-lg text-center navbar-light bg-light">
-                  <div class="container">
-                  <a class="navbar-brand" href="#">Navbar</a>
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav ml-auto">
-                      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                      <a class="nav-item nav-link" href="#">Features</a>
-                      <a class="nav-item nav-link" href="#">Pricing</a>
-                      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </div>
-                  </div>
-                </div>
-                </nav>
-            )
-          }
-      };
-      ReactDOM.render(<Header />, document.querySelector('body'));
-      ReactDOM.render(<Headers />, document.querySelector('body'));
-    </script>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
